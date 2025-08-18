c773234aa5cb6d377933d285dc456002aa59912f
Dax ten Voorde <85450505+daxinspark@users.noreply.github.com>
2025-08-18 16:05:37 +0200

    Committing 2 items from workspace ce7b25b6-db17-40c4-a36a-a2fb96314644

--- Fabric/Utils.Notebook/notebook-content.py
+++ Fabric/Utils.Notebook/notebook-content.py
@@ -6,7 +6,34 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "d30e8df9-0551-46b6-805e-cfc054b195be",
+# META       "default_lakehouse_name": "lh_davlo_config",
+# META       "default_lakehouse_workspace_id": "086e574d-a26f-4a9d-a34d-af3125924f27",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "d30e8df9-0551-46b6-805e-cfc054b195be"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# # Utils
+# xxx
+
+# CELL ********************
+
+%pip install pandas --quiet
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
 # META }
 
 # CELL ********************
@@ -22,14 +49,42 @@ from datetime import datetime, timedelta
 # META   "language_group": "synapse_pyspark"
 # META }
 
+# MARKDOWN ********************
+
+# ## Data security
+# xxx
+
 # CELL ********************
 
-def show_df(df):
-    display(df.head())
+%run nb_utils_data_security
 
 # METADATA ********************
 
 # META {
 # META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC -- Create ManagedWorkspaces table in Fabric Lakehouse
+# MAGIC CREATE TABLE ManagedWorkspaces (
+# MAGIC     WorkspaceID STRING,
+# MAGIC     WorkspaceName STRING,
+# MAGIC     WorkspaceURI STRING,
+# MAGIC     IsActive BOOLEAN,
+# MAGIC     CreatedOn timestamp,
+# MAGIC     ChangedOn timestamp,
+# MAGIC     OwnerEmail STRING,
+# MAGIC     Department STRING
+# MAGIC )
+# MAGIC USING DELTA;
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
 # META   "language_group": "synapse_pyspark"
 # META }
